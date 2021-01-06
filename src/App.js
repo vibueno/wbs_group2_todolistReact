dec1e6fe023969d0719907b7c66fd2b05ea04cbd
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-01-06 14:52:32 +0100

    ADD static layout to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import './App.css';
+import { TodoList } from './components/TodoList';
 import ToDoTask from "./components/todotask";
 
+const defaultTasks = [
+  {id:"1",title:"test1",checked:false},
+  {id:"2",title:"test2",checked:false},
+  {id:"3",title:"test3",checked:false}
+]
 export default function App() {
-
-
-
-
-
   return (
-    <div className="App">
-      <ToDoTask title="test"  id={1} />
-    </div>
+    <div class="main">
+          <h1>
+              <span>NOT</span>
+              <span>todo</span>
+              <span>LIST</span>
+          </h1>
+          <ol id="instructions">
+              <li>Write down things not to do.</li>
+              <li>
+                  If someone asks you to do said thing, just reply with: "Sorry,
+                  but that's on my Not Todo List."
+              </li>
+              <li>
+                  If you successfully avoided doing said thing, cross it of your
+                  list.
+              </li>
+          </ol>  
+          <TodoList tasks={defaultTasks}/>
+          <i class="license">
+              icons by fontawesome -{" "}
+              <a href="https://fontawesome.com/license">license</a>
+          </i>
+    </div>       
   );
 }
