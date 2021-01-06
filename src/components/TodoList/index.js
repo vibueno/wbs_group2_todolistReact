428b59c7c1c14120ef1f1425f82f8c595226bf29
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-01-06 14:51:40 +0100

    ADD TodoList Component

---
+++ src/components/TodoList/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import TodoTask from "../todotask";
+
+export function TodoList({ tasks }) {
+    const [taskList, setTaskList] = useState(tasks);
+    const deleteTask = (id) => {
+        console.log(id + " deleted(mock)");
+    };
+    const addTask = (title, id) => {
+        console.log(`added new task with title:${title} and id:${id}`);
+    };
+    const checkTask = (id) => {
+        console.log(id + " checked (mock)");
+    };
+    return (
+        <div>
+            <p>What are you NOT doing today:</p>
+            {/*<NewTaskForm addTaskHandler={addTask} />*/}
+            <p>Things not to do:</p>
+            <ul>
+                {taskList.map((task) => (
+                    <TodoTask
+                        key={task.id}
+                        id={task.id}
+                        title={task.title}
+                        onDelete={deleteTask}
+                    />
+                ))}
+            </ul>
+        </div>
+    );
+}
